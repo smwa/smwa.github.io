d51d7e6694fd723428e6ece1e61fface4405002c
Michael Smith <michael.smith.ok@gmail.com>
2022-09-08 14:45:00 -0500

    Rebuild cache on jekyll build

--- service-worker.js
+++ service-worker.js
@@ -16,7 +16,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = "precache-202209031606570500";
+const PRECACHE = "precache-{{ site.time | replace:' ','' | replace:':','' | replace:'-','' }}";
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
